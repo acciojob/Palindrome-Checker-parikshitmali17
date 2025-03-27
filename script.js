4395ceacc65e78149d28ba977ea96c4628121187
Parikshit Mali <parikshitmali17@gmail.com>
2025-03-27 19:44:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
-
+if(str.lowerCase()==str.lowerCase().spilt("").reverse()){
+	return true
+}else{
+	return false
+}
 }
 module.exports = palindrome
